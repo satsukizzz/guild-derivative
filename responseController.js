18b34c3e7162ec5832bd49d2a29251399c0eb781
satsukizzz <dn.asp01@gmail.com>
2021-08-14 23:01:56 +0900

    implement file loader

--- responseController.js
+++ responseController.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const waypointFiles = fs.readdirSync('./waypoint').filter(file => file.endsWith('.js'));
+
 class ResponseController {
   constructor() {
     this.modules = [];
@@ -5,7 +8,10 @@ class ResponseController {
   }
 
   load() {
-    this.modules.push(require('./waypoint'));
+    for (const waypoint of waypointFiles) {
+      // set a new item in the Collection
+      // modules must be a function with (request, response, args) arguments, which request and response are http module parameters
+      this.modules.push(require(`./waypoint/${waypoint}`));
   }
 } 
 
