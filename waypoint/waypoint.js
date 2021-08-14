e4ce0a3bacb9e4165b9c62f06d6b2039914015a2
satsukizzz <dn.asp01@gmail.com>
2021-08-14 23:04:29 +0900

    compiled ts file successfully

--- waypoint/waypoint.js
+++ waypoint/waypoint.js
@@ -1,11 +1,11 @@
 const waypoint = (request, response, args) => {
-  if (request.url == '/waypoint') {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Hey, welcome to the waypoint.");
-    response.end();
-    return true;
-  }
-  return false;
-}
-
+    if (request.url == '/waypoint') {
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write("Hey, welcome to the waypoint typescript version.");
+        response.end();
+        return true;
+    }
+    return false;
+};
 module.exports = waypoint;
+//# sourceMappingURL=waypoint.js.map
