c40bb2ca67c7369d8b1f8550d7c4ce1381f44cd6
satsukizzz <dn.asp01@gmail.com>
2021-08-27 19:39:35 +0900

    fix undefined error, while understanding optional chains...

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ const responseController = new ResponseController();
 
 const fs = require('fs');
 const http = require('http');
-if ('development' === process?.env?.TARGET_ENV) {
+if ('development' === process.env?.TARGET_ENV) {
   const secret = JSON.parse(fs.readFileSync('./secret.json'));
+} else {
+  const secret = null;
 }
 
 const Twitter = require('twitter');
 
 const twitterClient = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY || secret?.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET || secret?.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_KEY || secret?.TWITTER_ACCESS_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET || secret?.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY || secret.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET || secret.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_KEY || secret.TWITTER_ACCESS_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET || secret.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 twitterClient.get('favorites/list', function(error, tweets, response) {
@@ -40,16 +42,16 @@ http.createServer((request, response) => {
     response.end();
   }
 })
-.listen(process.env.PORT || secret?.PORT);
+.listen(process.env.PORT || secret.PORT);
 
 
 // discord bot server
 const {Client, Collection, Intents, WebhookClient} = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 const webhookClient = new WebhookClient({
-  id: process.env.DISCORD_ANNOUNCES_WEBHOOK_ID || secret?.DISCORD_ANNOUNCES_WEBHOOK_ID,
-  token: process.env.DISCORD_ANNOUNCES_WEBHOOK_TOKEN || secret?.DISCORD_ANNOUNCES_WEBHOOK_TOKEN,
-  url: `https://discord.com/api/webhooks/${process.env.DISCORD_ANNOUNCES_WEBHOOK_ID || secret?.DISCORD_ANNOUNCES_WEBHOOK_ID}/${process.env.DISCORD_ANNOUNCES_WEBHOOK_TOKEN || secret?.DISCORD_ANNOUNCES_WEBHOOK_TOKEN}`
+  id: process.env.DISCORD_ANNOUNCES_WEBHOOK_ID || secret.DISCORD_ANNOUNCES_WEBHOOK_ID,
+  token: process.env.DISCORD_ANNOUNCES_WEBHOOK_TOKEN || secret.DISCORD_ANNOUNCES_WEBHOOK_TOKEN,
+  url: `https://discord.com/api/webhooks/${process.env.DISCORD_ANNOUNCES_WEBHOOK_ID || secret.DISCORD_ANNOUNCES_WEBHOOK_ID}/${process.env.DISCORD_ANNOUNCES_WEBHOOK_TOKEN || secret.DISCORD_ANNOUNCES_WEBHOOK_TOKEN}`
 });
 
 client.commands = new Collection();
@@ -63,14 +65,14 @@ client.commands.set('server', {
 
 client.commands.set('adpost', {
   execute(message, args) {
-    if((process.env.DISCORD_COMMANDER_CHANNEL_ID || secret?.DISCORD_COMMANDER_CHANNEL_ID) !== message.channelId) {
+    if((process.env.DISCORD_COMMANDER_CHANNEL_ID || secret.DISCORD_COMMANDER_CHANNEL_ID) !== message.channelId) {
       return;
     }
     webhookClient.send(message.content.slice('yt adpost'.length));
   }
 });
 
-const prefix = process.env.DISCORD_PREFIX || secret?.DISCORD_PREFIX;
+const prefix = process.env.DISCORD_PREFIX || secret.DISCORD_PREFIX;
 
 client.once('ready', () => {
   console.log('bot is ready!');
@@ -96,4 +98,4 @@ client.on('message', async (message) => {
   return;
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN || secret?.DISCORD_BOT_TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN || secret.DISCORD_BOT_TOKEN);
